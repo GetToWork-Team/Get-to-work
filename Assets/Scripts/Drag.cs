b77302cfb5fac1bb0654c1d7a9f820ee2a310157
m.robion <m.robion@student.isartdigital.com>
2024-09-10 11:01:12 +0200

    [update]detection system done and reset position done

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class Drag : MonoBehaviour
 {
     private bool isInDrag = false;
+    private bool shouldReCenter = true;
     private Vector3 offset;
+    
+    public GameObject trashZone;
+    public GameObject yesZone;
+    public float resetSpeed = 0.1f;
 
     void Update()
     {
@@ -14,6 +19,38 @@ public class Drag : MonoBehaviour
             transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
         }
     }
+    private void FixedUpdate()
+    {
+        if (transform.position.x < trashZone.transform.position.x)
+        {
+            shouldReCenter = false;
+            Debug.Log("To trash");
+        }
+        else if (transform.position.x > yesZone.transform.position.x)
+        {
+            shouldReCenter = false;
+            Debug.Log("To yes");
+        }
+        else
+        {
+            shouldReCenter = true;
+        }
+    }
+
+
+    IEnumerator ResetPosition()
+    {
+        Vector3 curentPosition = transform.position;
+        Vector3 origin = new Vector3( 0, 0, 0);
+
+        for (float percent = 0; transform.position != origin; percent += resetSpeed)
+        {
+            transform.position = Vector3.Lerp(curentPosition, origin, percent);
+            yield return null;
+        }
+
+    }
+
 
 
     private void OnMouseDown()
@@ -25,6 +62,10 @@ public class Drag : MonoBehaviour
     private void OnMouseUp()
     {
         isInDrag = false;
+        if(shouldReCenter)
+        {
+            StartCoroutine(ResetPosition());
+        }
     }
 
 }
