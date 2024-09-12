f2f2f5617eafb604753c1efda458dd96a0f0a6ce
GuilleuxKen <k.guilleux@student.isartdigital.com>
2024-09-12 12:12:04 +0200

    Fix Transition to scene commit

--- Assets/Scripts/UI/Transition.cs
+++ Assets/Scripts/UI/Transition.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Transition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public CanvasGroup _CanvasGroup;
+
+    public bool fadeIn = false;
+    public bool fadeOut = false;
+
+    public float fadeSpeed;
+
+    private void Update()
     {
-        
+        if (fadeIn)
+        {
+            if(_CanvasGroup.alpha < 1)
+            {
+                _CanvasGroup.alpha += fadeSpeed * Time.deltaTime;
+                if(_CanvasGroup.alpha >= 1 )
+                {
+                    fadeIn = false;
+                    
+                }
+            }
+        }
+
+        if (fadeOut)
+        {
+            if (_CanvasGroup.alpha >= 1)
+            {
+                _CanvasGroup.alpha -= fadeSpeed * Time.deltaTime;
+                if (_CanvasGroup.alpha == 0)
+                {
+                    fadeOut = false;
+                    
+
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void FadeIn()
     {
-        
+        fadeIn = true;
+    }
+
+    public void FadeOut()
+    {
+        fadeOut = true;
     }
 }
