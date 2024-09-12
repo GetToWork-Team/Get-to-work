40a6b0668c5b4ca90510e5bc9efda157e5ccf255
m.robion <robion.mathieu.16@gmail.com>
2024-09-12 16:44:53 +0200

    [modified]Volume working

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -18,21 +18,21 @@ public class Settings : MonoBehaviour
 
     public void OnMusicSliderChange()
     {
-        musicValue = musicSlider.value;
+        SoundManager.instance.musicVolume = musicValue = musicSlider.value;
         PlayerPrefs.SetFloat("musicValue", musicValue);
         PlayerPrefs.Save();
     }
 
     public void OnSfxSliderChange()
     {
-        sfxValue = sfxSlider.value;
+        SoundManager.instance.sfxVolume = sfxValue = sfxSlider.value;
         PlayerPrefs.SetFloat("sfxValue", sfxValue);
         PlayerPrefs.Save();
     }
     public void Load()
     {
-        musicValue = PlayerPrefs.GetFloat("musicValue", 1f);
-        sfxValue = PlayerPrefs.GetFloat("sfxValue", 1f);
+        SoundManager.instance.musicVolume = musicValue = PlayerPrefs.GetFloat("musicValue", 1f);
+        SoundManager.instance.sfxVolume = sfxValue = PlayerPrefs.GetFloat("sfxValue", 1f);
 
         if(musicSlider)
             musicSlider.value = musicValue;
