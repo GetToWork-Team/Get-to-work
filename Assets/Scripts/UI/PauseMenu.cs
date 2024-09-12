b8337b5eb750268f3bc7a7157f591d95b1d7c07d
GuilleuxKen <k.guilleux@student.isartdigital.com>
2024-09-13 01:04:44 +0200

    Final commit of today

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,25 +1,60 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Collections;
 
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private Button _ReturnToMain;
+    [SerializeField] private Timer _Timer; // Assure-toi que ce Timer est utilisé ou sinon enlève-le
+    public Transform pointA;
+    public Transform pointB;
+    public float duration = 2.0f;
+
+    private float timeElapsed = 0.0f;
+    private bool isAnimating = false;
 
     private void OnEnable()
     {
         _ReturnToMain.onClick.AddListener(OnReturnToMenu);
+        StartCoroutine(AnimateTransition(transform.position, pointB.position, false));
     }
+
     private void OnDisable()
     {
-        _ReturnToMain.onClick.RemoveAllListeners();
+        _ReturnToMain.onClick.RemoveListener(OnReturnToMenu);
     }
 
     private void OnReturnToMenu()
     {
-        gameObject.SetActive(false);
-        Time.timeScale = 1.0f;
+        if (!isAnimating)
+        {
+            
+            StartCoroutine(AnimateTransition(transform.position, pointA.position, true));
+        }
     }
 
-    
+    private IEnumerator AnimateTransition(Vector3 startPosition, Vector3 endPosition, bool deactivateOnEnd)
+    {
+        isAnimating = true;
+        timeElapsed = 0.0f;
+
+        while (timeElapsed < duration)
+        {
+            timeElapsed += Time.deltaTime;
+            float t = timeElapsed / duration;
+            transform.position = Vector3.Lerp(startPosition, endPosition, t);
+            yield return null;
+        }
+
+        transform.position = endPosition; // S'assurer que la position finale est correcte
+
+        if (deactivateOnEnd)
+        {
+            gameObject.SetActive(false);
+        }
+
+        isAnimating = false;
+    }
 }
+
+
